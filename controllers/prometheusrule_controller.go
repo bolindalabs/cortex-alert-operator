c421035c7f46790cc315b772aa2704989a6dfbfc
Jannis Schaefer <jannis@schaefer.dev>
2021-02-09 03:13:01 +0100

    add some documentation

--- controllers/prometheusrule_controller.go
+++ controllers/prometheusrule_controller.go
@@ -18,6 +18,7 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -82,28 +83,47 @@ func (r *PrometheusRuleReconciler) Reconcile(req ctrl.Request) (ctrl.Result, err
 		for _, g := range rule.Spec.Groups {
 			if err := r.Cortex.SetRuleGroup(cortexNamespace, g); err != nil {
 				log.Error(err, "unable to set rule group")
+
+				if err := r.setStatus(ctx, rule, fmt.Sprintf("unable to set rule group: %v", err)); err != nil {
+					log.Error(err, "unable to set rule group")
+					return ctrl.Result{}, err
+				}
+				return ctrl.Result{}, err
+			}
+
+			if err := r.setStatus(ctx, rule, "synced"); err != nil {
+				log.Error(err, "unable to set status")
 				return ctrl.Result{}, err
 			}
 		}
 	}
 
-	newRule := rule.DeepCopy()
-	newRule.Status.SyncStatus = "Synced"
-	if err := r.Patch(ctx, newRule, client.MergeFrom(&rule)); err != nil {
-		return ctrl.Result{}, err
-	}
-
 	return ctrl.Result{}, nil
 }
 
+// setStatus sets PrometheusStatus.
+func (r *PrometheusRuleReconciler) setStatus(ctx context.Context, rule monitoringv1.PrometheusRule, status string) error {
+	newRule := rule.DeepCopy()
+	newRule.Status.SyncStatus = status
+	if err := r.Patch(ctx, newRule, client.MergeFrom(&rule)); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// hasFinalizer checks if PrometheusRule has our finalizer set.
 func (r *PrometheusRuleReconciler) hasFinalizer(rule monitoringv1.PrometheusRule) bool {
 	return containsString(rule.ObjectMeta.Finalizers, finalizerName)
 }
 
+// isDeletionScheduled checks if the current PrometheusRule is scheduled for deletion.
+// That means a deletion timestamp is set, but it is not completely deleted as there may be finalizers on that object.
 func (r *PrometheusRuleReconciler) isDeletionScheduled(rule monitoringv1.PrometheusRule) bool {
 	return !rule.ObjectMeta.DeletionTimestamp.IsZero()
 }
 
+// removeFinalizer removes our finalizer from the current PrometheusRule.
 func (r *PrometheusRuleReconciler) removeFinalizer(ctx context.Context, rule monitoringv1.PrometheusRule, log logr.Logger) error {
 	log.Info("Removing finalizer")
 
@@ -116,6 +136,7 @@ func (r *PrometheusRuleReconciler) removeFinalizer(ctx context.Context, rule mon
 	return nil
 }
 
+// addFinalizer patches the current PrometheusRule, so that it contains our finalizer.
 func (r *PrometheusRuleReconciler) addFinalizer(ctx context.Context, rule monitoringv1.PrometheusRule, log logr.Logger) error {
 	log.Info("Adding finalizer")
 
@@ -132,10 +153,6 @@ func (r *PrometheusRuleReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&monitoringv1.PrometheusRule{}).
 		Complete(r)
-}
-
-func (r *PrometheusRuleReconciler) deleteExternalResources(rule *monitoringv1.PrometheusRule) error {
-	return nil
 }
 
 func containsString(slice []string, s string) bool {
